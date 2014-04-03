9985875f5fda774c036b737e7fec32d95af58ec8
Jesse Donat <donatj@gmail.com>
2014-04-03 11:47:35 -0500

    fixed watch code

--- diskWatcher.php
+++ diskWatcher.php
@@ -7,30 +7,30 @@ $config = parse_ini_file('config.ini', true);
 
 $po = new donatj\Pushover($config['pushover']['api_key'], $config['pushover']['user_key']);
 
-$flags = new donatj\Flags();
-$min   = & $flags->int('min-free', null, 'Minimum amount free in gigs.');
-$path  = & $flags->string('path', '/', 'Path to watch disk space of.');
-$sleep = & $flags->int('sleep', 10, 'Seconds to sleep between disk checks.');
-$sleep = & $flags->int('sleep', 300, 'Seconds to sleep after a notification.');
+$flags      = new donatj\Flags();
+$min        = & $flags->uint('min-free', null, 'Minimum amount free in gigs.');
+$path       = & $flags->string('path', '/', 'Path to watch disk space of.');
+$sleep      = & $flags->uint('sleep', 10, 'Seconds to sleep between disk checks.');
+$note_sleep = & $flags->uint('sleep', 300, 'Seconds to sleep after a notification.');
 
 try {
-    $flags->parse();
+	$flags->parse();
 } catch(Exception $e) {
-    die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL);
+	die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL);
 }
 
 $min_bytes = $min * 1073741824;
 
-while(true) {
+while( true ) {
 	$free = disk_free_space($path);
 
-	if($free < $min_bytes) {
-		$po->send( 'Under minimum gigs on ' . gethostname(), array('priority' => 1, 'sound' => 'updown')) or die('Message Failed');
+	if( $free < $min_bytes ) {
+		$po->send('Under minimum gigs on ' . gethostname(), array( 'priority' => 1, 'sound' => 'updown' )) or die('Message Failed');
 		echo "notified\n";
-		sleep(300);
-	}else{
+		sleep($note_sleep);
+	} else {
 		echo '.';
-		sleep(10);
+		sleep($sleep);
 	}
 }
 
