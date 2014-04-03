5b533f8cb0958095b9a2202253d72d90277f7c0c
Jesse Donat <donatj@gmail.com>
2014-04-03 14:02:31 -0500

    Nicer output, Path as argument now

--- diskWatcher.php
+++ diskWatcher.php
@@ -9,15 +9,19 @@ $po = new donatj\Pushover($config['pushover']['api_key'], $config['pushover']['u
 
 $flags      = new donatj\Flags();
 $min        = & $flags->uint('min-free', null, 'Minimum amount free in gigs.');
-$path       = & $flags->string('path', '/', 'Path to watch disk space of.');
 $sleep      = & $flags->uint('sleep', 10, 'Seconds to sleep between disk checks.');
 $note_sleep = & $flags->uint('note-sleep', 300, 'Seconds to sleep after a notification.');
 
 try {
 	$flags->parse();
+	if(count($flags->args()) > 1) {
+		throw new Exception('Too many arguments');
+	}
 } catch(Exception $e) {
 	die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL);
 }
+
+$path = $flags->arg(0) ?: '/';
 
 $gig_bytes = 1073741824;
 $min_bytes = $min * $gig_bytes;
@@ -25,13 +29,16 @@ $min_bytes = $min * $gig_bytes;
 while( true ) {
 	$free = disk_free_space($path);
 
+	$readable_gb = intval($free / $gig_bytes);
 	if( $free < $min_bytes ) {
-		$po->send('Under minimum gigs on ' . gethostname(), array( 'priority' => 1, 'sound' => 'updown' )) or die('Message Failed');
-		echo "notified\n";
+		$po->send('Under minimum space for "'. $path .'" on ' . gethostname() . '. at ' . $readable_gb, array( 'priority' => 1, 'sound' => 'updown' )) or die('Message Failed');
+		echo $readable_gb . " - notified\n";
 		sleep($note_sleep);
 	} else {
-		echo intval($free / $gig_bytes) . ' ';
+		echo "\033[2K\0337";
+		echo "{$readable_gb} GB\t" . date("Y-m-d H:i:s");
 		sleep($sleep);
+		echo "\0338";
 	}
 }
 
