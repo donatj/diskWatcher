0166a1ba750fa3bf70ab4cd33b18dcda47624f3e
Jesse Donat <donatj@gmail.com>
2014-04-03 15:27:57 -0500

    Made accept multiple paths

--- diskWatcher.php
+++ diskWatcher.php
@@ -11,36 +11,39 @@ $po = new donatj\Pushover($config['pushover']['api_key'], $config['pushover']['u
 
 $flags      = new donatj\Flags();
 $min        = & $flags->uint('min-free', null, 'Minimum amount free in gigs.');
-$sleep      = & $flags->uint('sleep', 10, 'Seconds to sleep between disk checks.');
+$sleep      = & $flags->uint('sleep', 1, 'Seconds to sleep between disk checks.');
 $note_sleep = & $flags->uint('note-sleep', 300, 'Seconds to sleep after a notification.');
 
 try {
 	$flags->parse();
-	if(count($flags->args()) > 1) {
-		throw new Exception('Too many arguments');
-	}
 } catch(Exception $e) {
 	die($e->getMessage() . PHP_EOL . $flags->getDefaults() . PHP_EOL);
 }
 
-$path = $flags->arg(0) ?: '/';
+$paths      = $flags->args() ?: array('/');
+$path_count = count($paths);
+$sleep_time = $sleep;
 
 $gig_bytes = 1073741824;
 $min_bytes = $min * $gig_bytes;
 
 while( true ) {
-	$free = disk_free_space($path);
 
-	$readable_gb = intval($free / $gig_bytes);
-	if( $free < $min_bytes ) {
-		$po->send('Under minimum space for "'. $path .'" on ' . gethostname() . '. at ' . $readable_gb, array( 'priority' => 1, 'sound' => 'updown' )) or die('Message Failed');
-		echo $readable_gb . " - notified\n";
-		sleep($note_sleep);
-	} else {
-		echo "\033[2K\0337";
-		echo "{$readable_gb} GB\t" . date("Y-m-d H:i:s");
-		sleep($sleep);
-		echo "\0338";
+	foreach($paths as $path) {
+		$free = disk_free_space($path);
+
+		$readable_gb = intval($free / $gig_bytes);
+		if( $free < $min_bytes ) {
+			$po->send('Under minimum space for "'. $path .'" on ' . gethostname() . '. at ' . $readable_gb, array( 'priority' => 1, 'sound' => 'updown' )) or die('Message Failed');
+			echo $readable_gb . " - notified\n";
+			$sleep_time = $note_sleep;
+		} else {
+			echo "{$path}\t{$readable_gb} GB\t" . date("Y-m-d H:i:s") . "\n";
+			$sleep_time = $sleep;
+		}
 	}
-}
 
+	sleep($sleep_time);
+
+	echo "\033[{$path_count}A";
+}
